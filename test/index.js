066cedc5cc6086e3b840b7f8e164ecf95ea6a8fc
원더 <wonderbotsupport@naver.com>
2020-11-24 16:11:06 +0900

    test(shard): Added sharding command for test

--- test/index.js
+++ test/index.js
@@ -1,12 +1,12 @@
-const Discord = require('../../w-djs/src');
+const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config')
 
 const Dokdo = require('../src')
 
-const DokdoHandler = new Dokdo(client, { aliases: [ 'dokdo', 'dok' ], owners: [ "285185716240252929" ], prefix: '!', noPerm: (message) => message.reply('No Permission') })
+const DokdoHandler = new Dokdo(client, { aliases: [ 'dokdo', 'dok' ], owners: ['285185716240252929', '260303569591205888'], prefix: '!', noPerm: (message) => message.reply('No Permission') })
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`)
 });
 
 client.on('message', async message => {
@@ -14,4 +14,4 @@ client.on('message', async message => {
     
 }); 
 
-client.login(config.token);
+client.login(config.token)
