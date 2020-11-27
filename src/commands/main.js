bdbe007c8d32d5f600ef43a4e31fe14e5f2dd5fb
zero734kr <zero734kr@gmail.com>
2020-11-27 13:27:55 -0300

    feat: show RSS memory usage and percentage of using it by heap used

--- src/commands/main.js
+++ src/commands/main.js
@@ -7,21 +7,21 @@ const version = require('../../package.json').version
 
 module.exports = async function (message, parent) {
   let summary = `Dokdo v${version}, discord.js \`${parent.Discord.version}\`, \`Node.js ${process.version}\` on \`${process.platform}\`
-Process started at ${dayjs(system.processReadyAt()).fromNow()}, bot was ready at ${dayjs(parent.client.readyAt).fromNow()}.
+Process started at \`\`${dayjs(system.processReadyAt()).fromNow()}\`\`, bot was ready at \`\`${dayjs(parent.client.readyAt).fromNow()}\`\`.
 `
 
-  summary += `\nAssigned ${system.memory().rss} at this process and using ${system.memory().heapUsed} (${((process.memoryUsage().heapUsed / process.memoryUsage().rss) * 100).toFixed(2)}%).\n`
-  const cache = `${parent.client.guilds.cache.size} guild(s) and ${parent.client.users.cache.size} user(s)`
+  summary += `\nAssigned \`\`${system.memory().rss}\`\` at this process and using \`\`${system.memory().heapUsed} (${((process.memoryUsage().heapUsed / process.memoryUsage().rss) * 100).toFixed(2)}%)\`\`.\n`
+  const cache = `\`\`${parent.client.guilds.cache.size} guild(s)\`\` and \`\`${parent.client.users.cache.size} user(s)\`\``
 
   if (parent.client.shard) {
     const guilds = await parent.client.shard.fetchClientValues('guilds.cache.size').then(r => r.reduce((prev, val) => prev + val, 0))
-    summary += `Running on PID ${process.pid} for this client, and running on PID ${process.ppid} for the parent process.
+    summary += `Running on \`\`PID ${process.pid}\`\` for this shard, and running on \`\`PID ${process.ppid}\`\` for the parent process.
     
-This bot is sharded in ${Array.isArray(parent.client.shard.shards) ? parent.client.shard.shards.length : parent.client.shard.count} shard(s) and running in ${guilds} guild(s).
+This bot is sharded in \`\`${Array.isArray(parent.client.shard.shards) ? parent.client.shard.shards.length : parent.client.shard.count} shard(s)\`\` and running in \`\`${guilds} guild(s).\`\`
 Can see ${cache} in this shard.`
-  } else summary += `Running on PID ${process.pid}\n\nThis bot is not sharded and can see ${cache}.`
+  } else summary += `Running on \`\`PID ${process.pid}\`\`\n\nThis bot is not sharded and can see ${cache}.`
 
-  summary += `\nAverage websocket latency: ${parent.client.ws.ping}ms`
+  summary += `\nAverage websocket latency: \`\`${parent.client.ws.ping}ms\`\``
 
   return message.channel.send(summary)
 }
