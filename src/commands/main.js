623d10cb49ebef4888429503a808f7e7584c2501
원더 <wonderbotsupport@naver.com>
2020-11-27 13:37:42 +0900

    fix: fix to parent.client

--- src/commands/main.js
+++ src/commands/main.js
@@ -13,7 +13,7 @@ module.exports = async function (message, parent) {
 
   if (parent.client.shard) {
     const guilds = await parent.client.shard.fetchClientValues('guilds.cache.size').then(r => r.reduce((prev, val) => prev + val, 0))
-    summary += `Running on PID ${process.pid} for this client, and running on PID ${process.ppid} for the parent process.\n\nThis bot is sharded in ${Array.isArray(client.shard.shards) ? client.shard.shards.length : client.shard.count} shard(s) and running in ${guilds} guild(s).\nCan see ${cache} in this client.`
+    summary += `Running on PID ${process.pid} for this client, and running on PID ${process.ppid} for the parent process.\n\nThis bot is sharded in ${Array.isArray(parent.client.shard.shards) ? parent.client.shard.shards.length : parent.client.shard.count} shard(s) and running in ${guilds} guild(s).\nCan see ${cache} in this client.`
   } else summary += `Running on PID ${process.pid}\n\nThis bot is not sharded and can see ${cache}.`
 
   summary += `\nAverage websocket latency: ${parent.client.ws.ping}ms`
