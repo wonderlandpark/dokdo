e7737e6c3105ce81ee7e326bd48f1c495cd3d0ad
원더 <wonderbotsupport@naver.com>
2020-11-29 14:44:33 +0900

    fix: fix some

--- src/commands/shard.js
+++ src/commands/shard.js
@@ -6,19 +6,17 @@ module.exports = async function shard (message, parent) {
   if (!message.data.args) return message.channel.send('Missing Arguments.')
   if (!parent.client.shard) return message.channel.send('Shard Manager not found.')
   const res = parent.client.shard.broadcastEval(message.data.args)
-  const result = await res.then(r => r).catch(e => e)
-
+  const result = await res.catch(e => e.toString())
   let msg
-  console.log(result)
-  if (!Array.isArray(result)) msg = new ProcessManager(message, result, parent, { code: 'js' })
+  if (!Array.isArray(result)) msg = new ProcessManager(message, result, parent, { lang: 'js' })
   else {
     let sum
     if (typeof result[0] === 'number') sum = result.reduce((prev, val) => prev + val, 0)
     else if (result[0] instanceof Collection) sum = result.reduce((prev, val) => prev.concat(val))
 
-    msg = new ProcessManager(message, `// TOTAL\n${sum}\n\n${result.map((value, index) => `// #${index} SHARD\n${inspect(value)}`).join('\n')}`, parent, { code: 'js' })
+    msg = new ProcessManager(message, `// TOTAL\n${sum}\n\n${result.map((value, index) => `// #${index} SHARD\n${inspect(value)}`).join('\n')}`, parent, { lang: 'js' })
   }
 
   await msg.init()
-  await msg.addAction([{ emoji: '⏹️', action: ({ manager }) => manager.destroy() }, { emoji: '◀️', action: ({ manager }) => manager.previousPage(), requirePage: true }, { emoji: '▶️', action: ({ manager }) => manager.nextPage(), requirePage: true }])
+  await msg.addAction([{ emoji: '⏹️', action: ({ manager }) => manager.destroy(), requirePage: false }, { emoji: '◀️', action: ({ manager }) => manager.previousPage(), requirePage: true }, { emoji: '▶️', action: ({ manager }) => manager.nextPage(), requirePage: true }])
 }
