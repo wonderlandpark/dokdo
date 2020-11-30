7a4aee1931ce7b07571d0d7180aa752e5d269d00
zero734kr <zero734kr@gmail.com>
2020-11-30 16:03:46 -0300

    fix: the right-hand side is not compatible with value

--- src/utils/splitText.js
+++ src/utils/splitText.js
@@ -1,4 +1,4 @@
-module.exports = function splitText (text, { maxLength: 2000, char: '\n' } = {}) {
+module.exports = function splitText (text, { maxLength: 2000, char: '\n' }) {
   text = typeof text === 'string' ? text : Array.isArray(text) ? text.join('\n') : String(text)
   if (text.length < maxLength) return [text]
   const splitted = text.split(char)
