f9618ced54ba405de7892baffdb9eae9cb340661
zero734kr <zero734kr@gmail.com>
2020-11-30 16:15:52 -0300

    fix: pleeeease respect standard linting

--- src/utils/splitText.js
+++ src/utils/splitText.js
@@ -4,10 +4,12 @@ module.exports = function splitText (text, opt) {
   text = typeof text === 'string' ? text : Array.isArray(text) ? text.join('\n') : String(text)
   if (text.length < maxLength) return [text]
   const splitted = text.split(char)
-  const values = []
-  let msg = ''
-  for(const el of splitted) {
-    if((msg + char + el).length > maxLength) // what r u doing
+  // const values = []
+  const msg = ''
+  for (const el of splitted) {
+    if((msg + char + el).length > maxLength) {
+        return
+    } // what r u doing
     return // to pass eslint error
   }
 }
