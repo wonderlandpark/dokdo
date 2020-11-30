bd90ea5d083c44c90b4d4af335c4bc0fdcdff452
zero734kr <zero734kr@gmail.com>
2020-11-30 16:07:33 -0300

    fix: eslint error

--- src/utils/splitText.js
+++ src/utils/splitText.js
@@ -1,4 +1,6 @@
-module.exports = function splitText (text, { maxLength: 2000, char: '\n' }) {
+module.exports = function splitText (text, opt) {
+  const { maxLength, char } = { maxLength: opt.maxLength || 2000, char: opt.char || '\n' }
+
   text = typeof text === 'string' ? text : Array.isArray(text) ? text.join('\n') : String(text)
   if (text.length < maxLength) return [text]
   const splitted = text.split(char)
