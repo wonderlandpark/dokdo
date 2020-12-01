3ca87a3facfb2ebf7f669758dcf0a329e05034ae
원더 <wonderbotsupport@naver.com>
2020-12-01 11:36:45 +0900

    feat: js improved display string, function, error

--- src/commands/js.js
+++ src/commands/js.js
@@ -1,8 +1,9 @@
 const { ProcessManager, inspect, isinstance } = require('../utils')
+const Discord = require('discord.js')
 
 module.exports = async function js (message, parent) {
   // eslint-disable-next-line no-unused-vars
-  const { client, Discord } = parent // for eval
+  const { client } = parent // for eval
   if (!message.data.args) return message.channel.send('Missing Arguments.')
 
   // eslint-disable-next-line no-eval
@@ -13,11 +14,17 @@ module.exports = async function js (message, parent) {
       typeOf = typeof output
       if (output instanceof Discord.MessageEmbed) await message.channel.send(output)
       else if (isinstance(output, Discord.MessageAttachment)) await message.channel.send({ files: output instanceof Discord.Collection ? output.array() : [output] })
-      if (typeof output === 'function') output = output.toString()
+      if (typeof output === 'function') {
+        typeOf = 'object'
+        return output.toString()
+      } else if (typeof output === 'string') {
+        return output
+      }
       message.react('✅')
-      return inspect(output, { depth: 1, maxStringLength: 300, maxArrayLength: 200 })
+      return inspect(output, { depth: 1, maxArrayLength: 200 })
     })
     .catch(e => {
+      typeOf = 'object'
       message.react('❗')
       return e.toString()
     })
