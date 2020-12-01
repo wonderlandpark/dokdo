0eb6ee67218660702314eeded35bad3f647e38af
원더 <wonderbotsupport@naver.com>
2020-12-01 11:37:08 +0900

    feat: added Content Type

--- src/commands/jsi.js
+++ src/commands/jsi.js
@@ -1,8 +1,9 @@
-const { ProcessManager, inspect, table, type } = require('../utils')
+const { ProcessManager, count, inspect, table, type } = require('../utils')
+const Discord = require('discord.js')
 
 module.exports = async function jsi (message, parent) {
   // eslint-disable-next-line no-unused-vars
-  const { client, Discord } = parent
+  const { client } = parent
   if (!message.data.args) return message.channel.send('Missing Arguments.')
 
   // eslint-disable-next-line no-eval
@@ -12,9 +13,10 @@ module.exports = async function jsi (message, parent) {
     const typeofTheRes = type(output)
     const overview = inspect(output, { depth: -1 })
     const constructorName = output && output.constructor ? Object.getPrototypeOf(output.constructor).name : null
-    msg = new ProcessManager(message, `=== ${overview.slice(0, 100)}${overview.length > 100 ? '...' : ''} ===\n\n${table({ Type: `${typeof output}(${typeofTheRes})`, Name: constructorName || null, Length: typeof output === 'string' && output.length })}`, parent, { lang: 'prolog' })
+    const arrCount = count(output)
+    msg = new ProcessManager(message, `=== ${overview.slice(0, 100)}${overview.length > 100 ? '...' : ''} ===\n\n${table({ Type: `${typeof output}(${typeofTheRes})`, Name: constructorName || null, Length: typeof output === 'string' && output.length, Size: output instanceof Discord.Collection ? output.size : null, 'Content Types': arrCount ? arrCount.map(el => `${el.name} (${el.ratio}％)`).join(', ') : null })}`, parent, { lang: 'prolog' })
   }).catch(e => {
-    msg = new ProcessManager(message, e.toString(), parent, { lang: 'js' })
+    msg = new ProcessManager(message, e.stack, parent, { lang: 'js' })
   })
 
   await msg.init()
