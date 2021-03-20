544665aeac679b7add948c8e80eb9d3b46d76fa1
wonderlandpark <wonderbotsupport@naver.com>
2021-03-20 18:59:50 +0900

    chore: changed emoji order

--- src/commands/curl.js
+++ src/commands/curl.js
@@ -24,5 +24,5 @@ module.exports = async function curl (message, parent) {
   // console.log(res)
   const msg = new ProcessManager(message, res || '', parent, { lang: type })
   await msg.init()
-  await msg.addAction([{ emoji: '⏹️', action: ({ manager }) => manager.destroy(), requirePage: true }, { emoji: '◀️', action: ({ manager }) => manager.previousPage(), requirePage: true }, { emoji: '▶️', action: ({ manager }) => manager.nextPage(), requirePage: true }])
+  await msg.addAction([{ emoji: '◀️', action: ({ manager }) => manager.previousPage(), requirePage: true }, { emoji: '⏹️', action: ({ manager }) => manager.destroy(), requirePage: true }, { emoji: '▶️', action: ({ manager }) => manager.nextPage(), requirePage: true }])
 }
