f5f2ef40a69cb3227fb97015badb9140dce016f9
KeepSOBP <osu@kakao.com>
2021-08-25 15:43:34 +0900

    fix(djsDocs): change branch from master to main (#298)

--- src/utils/djsDocs.js
+++ src/utils/djsDocs.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 
 module.exports.source = version => {
   const parsed = version.match(/^(\d+)\.\d+\.\d+$/)?.[1]
-  return `https://raw.githubusercontent.com/discordjs/discord.js/docs/${parsed ? `v${parsed}` : 'master'}.json`
+  return `https://raw.githubusercontent.com/discordjs/discord.js/docs/${parsed ? `v${parsed}` : 'main'}.json`
 }
 
 module.exports.docs = async (q) => {
