3231afdc9b5fdb4f6fbbea95d4b9ea34248595da
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2021-09-12 13:02:37 +0800

    fix(docs): fixed not found error

--- src/utils/djsDocs.js
+++ src/utils/djsDocs.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js')
 
 module.exports.source = version => {
   const parsed = version.match(/^(\d+)\.\d+\.\d+$/)?.[1]
-  return `https://raw.githubusercontent.com/discordjs/discord.js/docs/${parsed ? `v${parsed}` : 'main'}.json`
+  return `https://raw.githubusercontent.com/discordjs/discord.js/docs/${parsed ? `v${parsed}` : 'stable'}.json`
 }
 
 /**
@@ -14,7 +14,7 @@ module.exports.docs = async (q) => {
   const params = new URLSearchParams({ force: true, src: this.source(Discord.version), q })
   const res = await fetch(`https://djsdocs.sorta.moe/v2/embed?${params}`)
   const embed = await res.json()
-  console.log(embed)
-  if (!embed) return 'Nothing found'
+  //console.log(embed)
+  if (!embed) return { content: 'Nothing found' }
   else return { embeds: [embed] }
 }
