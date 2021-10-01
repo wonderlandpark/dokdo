be2dd8e7fc7d1a3e7ff92c0b2c778388ec2a8be6
Junseo Park <wonderlandpark@outlook.kr>
2021-10-01 22:50:45 +0900

    Fixing error on docs command (#324)
    * fix(docs): checking if result is not array
    
    * fix(djsdocs): return main branch if version includes dev (#325)
    
    * fix(djsdocs): return main branch if version includes dev
    
    * chore(djsdocs): doublequote to singlequote
    
    * fix: sending nothing found even returns normarly
    
    * chore: removed debug code
    
    Co-authored-by: KeepSOBP <osu@kakao.com>

--- src/utils/djsDocs.js
+++ src/utils/djsDocs.js
@@ -2,8 +2,9 @@ const fetch = require('node-fetch')
 const Discord = require('discord.js')
 
 module.exports.source = version => {
-  const parsed = version.match(/^(\d+)\.\d+\.\d+$/)?.[1]
-  return `https://raw.githubusercontent.com/discordjs/discord.js/docs/${parsed ? `v${parsed}` : 'main'}.json`
+  const isDev = version.includes('dev')
+  const parsed = version.match(/^(\d+)\.\d+\.\d+$/)?.[0]
+  return `https://raw.githubusercontent.com/discordjs/discord.js/docs/${isDev ? 'main' : parsed}.json`
 }
 
 /**
@@ -14,7 +15,6 @@ module.exports.docs = async (q) => {
   const params = new URLSearchParams({ force: true, src: this.source(Discord.version), q })
   const res = await fetch(`https://djsdocs.sorta.moe/v2/embed?${params}`)
   const embed = await res.json()
-  console.log(embed)
   if (!embed) return { content: 'Nothing found' }
   else return { embeds: [embed] }
 }
